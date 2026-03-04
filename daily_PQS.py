9006e6d334fcb46b98228b0935a9dc4ed765efe8
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-05 02:01:33 +0530

    python day-2PQS..

--- daily_PQS.py
+++ daily_PQS.py
@@ -42,3 +42,31 @@ Twinkle, twinkle, little star. """)
 #chapter-2
 # day-2 python learning 
 
+#PQS-1 : sun of two numbers ?
+
+num_a = 10
+num_b = 20
+
+print(num_a+num_b)
+
+#PQS-2 : print the reminder of num_a, num_b ?
+
+print(num_a%num_b)
+
+# PQS-3 : check the input type ?
+
+names =input("enter your nsme : ")
+print(f"name is: {names}")
+print(type(names))
+
+
+# PQS-4 Check the conditation is true or false (a>b)?
+A_n= input("enter your 1st number:")
+secnd= input("enter your 2nd number :")
+print(A_n>secnd)
+
+#PQS-5 find the average of a,b?
+avg1= int(input("enter your number 1 :"))
+avg2= int(input("enter your number 2 : "))
+
+print("the average is :", (avg1 + avg2)/2)
