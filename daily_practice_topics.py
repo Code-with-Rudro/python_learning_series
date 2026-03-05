7bdf1e98c92f77238f39e243cba533a9e8283dda
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-05 22:49:06 +0530

    python day-2 string fun..

--- daily_practice_topics.py
+++ daily_practice_topics.py
@@ -1,5 +1,8 @@
 #chapter-1
 # day-1 python learning seties
+import string
+
+
 print("hello world")
 print("hello")
 # hello
@@ -28,3 +31,31 @@ print(type(a))
 #input function
 name = input("enter your name")
 print (f"hello : {name}")
+
+
+#CHAPTER-3
+# day-3 python learning series
+#strings in python :
+
+# we can use (``), (""), ("""    """)
+
+str="rudra"
+print(str[3])
+
+#str[str_idx: end_idx]
+
+#nagative slicing :
+
+print(str[-2: -1])
+
+# skip value in string 
+
+# print(str[st_idx:end_idx:skip_size])
+
+print(str[0:4:1])
+
+
+# string functions :
+#length
+print(len(str))
+
