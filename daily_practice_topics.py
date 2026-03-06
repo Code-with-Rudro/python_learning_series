abaee7b0a2a7b23a70cd4de0c520689bd6b07f27
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-06 21:12:57 +0530

    python day-2PQS chapter4 py..

--- daily_practice_topics.py
+++ daily_practice_topics.py
@@ -66,3 +66,30 @@ print(len(str))
 #\': single quote
 #\\ : backslash
 print("heloo every one \n my self rudra \t \"i\" am learning python \n\t by code with harry")
+
+#CHAPTER 4
+# day-4 python learning series
+# LIST METHOD      |    TUPLE METHOD
+
+#list method :
+#list is a container it will store diffrent type of data type, list are mutable(changeable),
+list1 = ["harry", "kiran", 3, True]
+#methods
+#append 
+print(list1.append("rudra"))
+#remove
+print(list1.remove("kiran"))
+#pop()
+print(list1.pop(2))
+#short
+list2  = [1,20,3,4,54,7,11]
+list2.sort()
+print(list2)
+
+
+#tuple method :
+#tuple is a container it will store diffrent type of data type, tuple are immutable(unchangeable),
+list3 = ("pramod", "gorav", 542, False)
+
+print(list3)
+
