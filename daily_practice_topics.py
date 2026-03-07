a1e8e1cd5a39c0eb7021bb7a5d9b3ced11e70a55
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-07 20:12:27 +0530

    python day-3 chapter 5 sets ...

--- daily_practice_topics.py
+++ daily_practice_topics.py
@@ -131,3 +131,33 @@ print(mark.get("kiran")) # print none
 print(mark["pramod"]) # it print error
 print(mark["kiran"])
 
+#Set in python :
+# set is a container it will store diffrent type of data type, set are immutable(unchangeable), set are unordered, set cannot contain duplicate values.
+#ex - 
+s = {1,2,"jeneel",True}
+#empty set :
+empty_set = set()
+
+# properties of sets :-
+
+    # 1. set are unordered
+    # 2. set are unindexed
+    # 3. there is no way to change items in sets.
+    # 4. set are cannot containe duplicate values.
+
+
+# Set methods :
+
+#add() helps to add 
+set1={10,20,30,40,"rudra"}
+
+set1.add(2)
+print(set1)
+
+#union():-
+st1 = {1,2,3,4}
+st2 = {3,4,5,6,7}
+print(st1.union(st2))
+
+#intersectuon :-
+print(st1.intersection(st2))
