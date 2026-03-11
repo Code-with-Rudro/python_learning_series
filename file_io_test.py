91415d74cd2f012eb1b06e9c3082fab428133166
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-11 21:51:51 +0530

    py learning file i/o oprt pqs game.

--- file_io_test.py
+++ file_io_test.py
@@ -1,3 +1,4 @@
+#pqs-2  file handling   chapter-9
 word = "donkey"
 
 with open("io_test.txt", "r") as f:
@@ -7,3 +8,15 @@ contentnew = content.replace(word, "#####")
 
 with open("io_test.txt", "w") as f:
    print(f.write(contentnew))
+
+
+#pqs-3
+#change a list in the text file?
+words = ["help","hope","me"]
+
+with open("io_test.txt") as f:
+   conten = f.read()
+
+cont = conten.replace(word, "@@@@@"*len(words))
+with open("io_test.txt", "w") as f:
+   f.write(cont)
