b863935c0170d6147ce7de1dfe4da8c2ac3e56db
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-13 22:40:49 +0530

    OOPS multilevel inheritance

--- chapter_11.py
+++ chapter_11.py
@@ -53,3 +53,27 @@ c.feature4()
 c.feature5()
 c.feature6()
 
+#multilevel inheritance
+class A:
+    def feature1(self):
+        print("feature 1 is working")
+    def feature2(self):
+        print("feature 2 is working")
+class B(A):
+    def feature3(self):
+        print("feature 3 is working")
+    def feature4(self):
+        print("feature 4 is working")
+class C(B):
+    def feature5(self):
+        print("feature 5 is working")
+    def feature6(self):
+        print("feature 6 is working")
+c = C()
+c.feature1()
+c.feature2()
+c.feature3()
+c.feature4()
+c.feature5()
+c.feature6()
+
