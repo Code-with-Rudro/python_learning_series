5204f7261ae0167dd4e9daf0f8fb566b59e9bdf2
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-13 22:39:33 +0530

    OOPS

--- chapter_11.py
+++ chapter_11.py
@@ -28,3 +28,28 @@ b.feature2()
 b.feature3()
 b.feature4()
 
+#multiple inheritance
+class A:
+    def feature1(self):
+        print("feature 1 is working")
+    def feature2(self):
+        print("feature 2 is working")
+class B:
+    def feature3(self):
+        print("feature 3 is working")
+    def feature4(self):
+        print("feature 4 is working")
+class C(A,B):
+    def feature5(self):
+        print("feature 5 is working")
+    def feature6(self):
+        print("feature 6 is working")
+
+c = C()
+c.feature1()
+c.feature2()
+c.feature3()
+c.feature4()
+c.feature5()
+c.feature6()
+
