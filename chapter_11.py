2fbdace82dabd29e07ad6ac5e1269868343cc6e1
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-13 23:45:48 +0530

    OOPS chapter - 11 ...

--- chapter_11.py
+++ chapter_11.py
@@ -143,3 +143,54 @@ b.e = 20
 b.ename = "kiran seera"
 print(b.ename)
 b.show()
+
+
+#operator overloading:
+# operator overloading is a feature in python that allows us to define the behavior of an operator for a user-defined class. It is done by defining special methods in the class. These special methods are called magic methods or dunder methods.
+#types of operator overloading:
+#addition operator overloading 
+# example: 
+# the __add__ method is used to overload the addition operator. It takes two arguments, self and other. The self argument is the instance of the class and the other argument is the instance of the class that we want to add.
+#the__mul__ method is used to overload the multiplication operator. It takes two arguments, self and other. The self argument is the instance of the class and the other argument is the instance of the class that we want to multiply.
+#the __str__ method is used to overload the string representation of the class. It takes one argument, self. The self argument is the instance of the class. It returns a string representation of the class.
+#the __len__ method is used to overload the length of the class. It takes one argument, self. The self argument is the instance of the class. It returns the length of the class.
+#the __eq__ method is used to overload the equality operator. It takes two arguments, self and other. The self argument is the instance of the class and the other argument is the instance of the class that we want to compare.
+#the __lt__ method is used to overload the less than operator. It takes two arguments, self and other. The self argument is the instance of the class and the other argument is the instance of the class that we want to compare.
+#the __gt__ method is used to overload the greater than operator. It takes two arguments, self and other. The self argument is the instance of the class and the other argument is the instance of the class that we want to compare.
+#the __sub__ method is used to overload the subtraction operator. It takes two arguments, self and other. The self argument is the instance of the class and the other argument is the instance of the class that we want to subtract.
+
+#ex -
+class A:
+    def __init__(self,n):
+        self.n = n
+    def __add__(self, other):
+        return self.n + other.n
+a1 = A(10)
+a2 = A(20)
+print(a1 + a2)
+
+
+    
+        
+
+        
+#chapter-11
+#practice questions:
+#pqs-1
+#write a method salaryafterincriment method with @property decorator with setter which changes the salary of an employee after increment?
+
+class employes:
+    salary = 12000
+    incriment = 20
+    @property
+    def salaryafterincriment(self):
+        return (self.salary + self.salary *( self.incriment/100))
+    @incriment.setter
+
+    def salaryafterincriment(self, salary):
+        self.incriment = ((salary - self.salary)-1)*100
+
+emp = employes()
+print(emp.salaryafterincriment)
+emp.salaryafterincriment = 15000
+print(emp.incriment)
