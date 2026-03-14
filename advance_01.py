4bb1718e8628a51a2406ac80dda8979bb0945a89
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-14 23:28:36 +0530

    advance py chptr - 12.

--- advance_01.py
+++ advance_01.py
@@ -13,3 +13,34 @@ if n > 10:
 # with walrus operator
 if (n := int(input("Enter a number: "))) > 10:
     print(f"{n} is greater than 10")
+
+
+#types definations:
+#type hints are added using the colon(:) syn for variable and the -> syn for function return tyoe.
+
+# ex:-
+
+from typing import List 
+
+numbers : list[int] = [1,2,3,4]
+
+print(numbers)
+
+#this annotations helps to making the code self documenting and allow devlopers to understand the data structures used at a glance.
+
+
+#MATCH catch :
+# its involvs matching a variable against several cases using this "case " keyword.
+
+def status(st):
+    match st:
+        case 100:
+            return "lower value"
+        case 200:
+            return "middle value"
+        case 500:
+            return "higher value"
+        case _:
+            return "none"
+
+print(status(100))
